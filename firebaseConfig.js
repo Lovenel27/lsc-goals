e451b15c82a0bb7b1525ec6e875820fd088d057e
Lovenel27 <lovenelcanillo@gmail.com>
2025-08-27 09:47:15 +0800

    Firebase config

---
+++ firebaseConfig.js
@@ -0,0 +1,14 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCq7NYw2DcQY0zudNrwpmZxKC4m_iH1KiY",
+  authDomain: "lsc-goals.firebaseapp.com",
+  projectId: "lsc-goals",
+  storageBucket: "lsc-goals.firebasestorage.app",
+  messagingSenderId: "493458421040",
+  appId: "1:493458421040:web:874c49d538f845550284f5"
+};
+
+
+const app = initializeApp(firebaseConfig);
